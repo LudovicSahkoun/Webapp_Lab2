178ed75cc99423c4f4f27055b3c9c0af6eee287c
LudovicSahkoun <101654780+LudovicSahkoun@users.noreply.github.com>
2022-11-13 15:50:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask,Response,request
+from myapp import *
+app = Flask(__name__)
 
+
+
+@app.route('/home/<lower>/<upper>')
+def create(lower,upper):
+    result=numerical_integration(float(lower),float(upper))
+    return "<h1>%s</h1>" % str(result)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
